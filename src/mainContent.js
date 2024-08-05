5e8e8ff9634e6a75457b9329506ba87bf7d8ed78
misafxd <pisichu.mafs@gmail.com>
2024-08-05 17:57:48 -0600

    Add new task button to main page

--- src/mainContent.js
+++ src/mainContent.js
@@ -1,11 +1,22 @@
 import { Button } from "./button";
+import plus from './plus.png';
 
 export function MainContent(){
 
     const main = document.getElementById('main');
-    const mainHeader = document.createElement('h2');
+    const mainHeader = document.createElement('div');
+    const mainTitle = document.createElement('h2');
     mainHeader.classList.add('main-header');
-    mainHeader.textContent = "All";
+    mainTitle.textContent = "All";
+    const plusIcon = new Image();
+    plusIcon.src = plus;
+
+    const addTaskButton = Button(()=> {console.log("new task")},"New task", plusIcon);
+    // addTaskButton.classList.remove('aside-button');
+    addTaskButton.classList.add('main-button');
+
+    mainHeader.appendChild(mainTitle);
+    mainHeader.appendChild(addTaskButton);
     
 
 
